baf1f6445f01716d34a45de42ad19959c1c30867
Yosip Mike Colin <vendeyaonline10@gmail.com>
2024-08-06 17:56:47 -0500

    Settings text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import 'animate.css';
 
 const inter = Poppins({ subsets: ["latin"], weight: "300" });
 
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -9,18 +9,20 @@ const poppins = Poppins({ subsets: ["latin"], weight: "500" });
 
 const Home = () => {
   useEffect(() => {
-    const image = document.querySelector(`.${classes["header-image"]}`);
-    const textElement = document.querySelector(`.${classes["header-text"]}`);
-    if (image && textElement) {
+    const image = document.querySelector(`.${classes["home-image"]}`);
+    const textElement = document.querySelector(`.${classes["home-text"]}`);
+    const textFooter = document.querySelector(`.${classes["footer-home"]}`);
+    if (image && textElement && textFooter) {
       image.classList.add(classes["zoom-in"]);
       textElement.classList.add(classes["animate-text"]);
+      textFooter.classList.add(classes["footer-home-animation"]);
     }
   }, []);
 
   return (
-    <section className={classes["container-header"]}>
+    <section className={classes["container-home"]}>
       <div className={classes["container-text"]}>
-        <h1 className={`${poppins.className} ${classes["header-text"]}`}>
+        <h1 className={`${poppins.className} ${classes["home-text"]}`}>
           Diseñando espacios,
           <br /> construyendo sueños
         </h1>
@@ -29,8 +31,27 @@ const Home = () => {
         src={ImageHeader}
         alt="imagen de inicio"
         priority
-        className={classes["header-image"]}
+        className={classes["home-image"]}
       />
+      <div className={classes["footer-home"]}>
+        <p className="w-[600px] mt-10">
+          Arquitecto recién graduado con experiencia en diseño y planificación
+          de espacios. Me apasiona crear entornos que mejoran la calidad de vida
+          de las personas.
+        </p>
+        <button className={classes["button-contact"]}>Contactar</button>
+      </div>
+
+      <div
+        className={`animate__animated animate__fadeInUp animate__delay-1s ${classes["footer-home-mobil"]}`}
+      >
+        <p>
+          Arquitecto recién graduado con experiencia en diseño y planificación
+          de espacios. Me apasiona crear entornos que mejoran la calidad de vida
+          de las personas.
+        </p>
+        <button className={classes["button-contact"]}>Contactar</button>
+      </div>
     </section>
   );
 };
